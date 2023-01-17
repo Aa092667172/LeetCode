0efc82d9a236ffe42341b9c8860be6908a6c8bfd
Aa092667172 <ttaehee520@gmail.com.tw>
2023-01-17 20:25:47 +0800

    283

--- src/main/java/easy/$283MoveZeroes.java
+++ src/main/java/easy/$283MoveZeroes.java
@@ -23,12 +23,20 @@ import java.util.Map;
  */
 public class $283MoveZeroes {
     public void moveZeroes(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-           if(nums[i]==0){
-               System.out.println(i);
-              nums[i]=nums[i+1];
-              nums[i+1]=0;
-           }
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                count++;
+            } else if (count > 0) {
+                int temp = nums[i];
+                nums[i] = 0;
+                nums[i - count] = temp;
+            }
         }
+    }
+
+    public static void main(String[] args) {
+        $283MoveZeroes $283MoveZeroes = new $283MoveZeroes();
+        $283MoveZeroes.moveZeroes(new int[]{0,1,0,3,12});
     }
 }

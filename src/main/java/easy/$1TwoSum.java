ea79115bd4dc8556c75f9c3895ec907b9805c760
kevin <kevinlin@iftek.com.tw>
2023-04-24 23:04:04 +0800

    排版

--- src/main/java/easy/$1TwoSum.java
+++ src/main/java/easy/$1TwoSum.java
@@ -12,16 +12,16 @@ import java.util.Map;
 
 public class $1TwoSum {
 	public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> map = new HashMap<>();
-		 
-		 for(int i=0;i<nums.length;i++) {
-			 int result = target-nums[i];
-			 if(map.containsKey(result)) {
-				 return new int [] {map.get(result),i};
-			 }
-			 map.put(nums[i],i);
-		 }
-		 
-		 return new int[] {};
+		Map<Integer,Integer> map = new HashMap<>();
+
+		for(int i=0;i<nums.length;i++) {
+			int result = target-nums[i];
+			if(map.containsKey(result)) {
+				return new int [] {map.get(result),i};
+			}
+			map.put(nums[i],i);
+		}
+
+		return new int[] {};
     }
 }
